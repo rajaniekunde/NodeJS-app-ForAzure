5e31de583d603670f9cb0385d977c6ae090ec293
rajaniekunde <151899905+rajaniekunde@users.noreply.github.com>
2024-11-28 01:27:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello, Azure!\n');
+    res.end('Hello, Globallogic!\n');
 });
 
 server.listen(PORT, () => {
